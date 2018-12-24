54195ecf0c1a533412b0f63b5b76572fb138d6ba
Rommel Rico <me@rommelrico.com>
2018-12-24 13:47:33 -0800

    Testing an onClick event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,13 +6,17 @@ class SearchBar extends React.Component {
         console.log("Here: " + event.target.value);
     }
 
+    onInputClick() {
+        console.log("Input was clicked");
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <input type="text" onChange={this.onInputChange} onClick={this.onInputClick} />
                     </div>
                 </form>
             </div>
