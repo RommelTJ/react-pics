158536f27cf965ec69dea20bd5ac0aedbc63088b
Rommel Rico <me@rommelrico.com>
2018-12-24 13:54:45 -0800

    Reverting the abbreviated syntax

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,8 +12,9 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        {/*<input type="text" onChange={this.onInputChange} />*/}
-                        <input type="text" onChange={(e) => console.log("Here: " + e.target.value)} />
+                        <input type="text" onChange={this.onInputChange} />
+                        {/*Alternate syntax:*/}
+                        {/*<input type="text" onChange={(e) => console.log("Here: " + e.target.value)} />*/}
                     </div>
                 </form>
             </div>
