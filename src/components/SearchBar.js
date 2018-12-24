be44877a86d7876d32752c2a8bbfbe462e3e0120
Rommel Rico <me@rommelrico.com>
2018-12-24 14:02:27 -0800

    Setting term onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = { term: '' };
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <input type="text" onChange={(e) => this.setState({ term: e.target.value })} />
                         {/*Alternate syntax:*/}
                         {/*<input type="text" onChange={(e) => console.log("Here: " + e.target.value)} />*/}
                     </div>
