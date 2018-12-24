a4908a5c4727c33da982b775b8fbbf28186b7381
Rommel Rico <me@rommelrico.com>
2018-12-24 14:55:30 -0800

    Code styling

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="ui container" style={{ marginTop: '10px' }}><SearchBar/></div>
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar/>
+            </div>
         );
     }
 }
