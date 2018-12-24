5a42fbdf4746b86998d5ccab4cd5b8618fbf87d7
Rommel Rico <me@rommelrico.com>
2018-12-24 14:03:26 -0800

    Adding a value prop

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={(e) => this.setState({ term: e.target.value })} />
+                        <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
                     </div>
                 </form>
             </div>
