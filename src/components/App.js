0158ada61c068e82c2c8f86b3af49f5679896a62
Rommel Rico <me@rommelrico.com>
2018-12-25 10:25:04 -0800

    Converting onSearchSubmit to an arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
 
     state = { images: [] };
 
-    async onSearchSubmit(searchTerm) {
+    onSearchSubmit = async (searchTerm) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             headers: {
                 Authorization: 'Client-ID REDACTED'
@@ -17,7 +17,7 @@ class App extends React.Component {
         });
 
         this.setState({ images: response.data.results });
-    }
+    };
 
     render() {
         return (
