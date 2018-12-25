730cefec248e4c9d8117c78634c60fd6d756547c
Rommel Rico <me@rommelrico.com>
2018-12-25 10:17:31 -0800

    Setting the state

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
             }
         });
 
-        console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
 
     render() {
