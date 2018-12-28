78297fb7c124f306689d2126ce70f96c66ac2786
Rommel Rico <me@rommelrico.com>
2018-12-27 19:59:30 -0800

    Creating a Ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 class ImageCard extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
     render() {
         const { description, urls } = this.props.image;
 
